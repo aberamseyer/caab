c6da0575b4a7f307e167447b194c55b1c4a77e87
Manny, Harry <himanny@ilstu.edu>
2017-10-27 19:50:10 -0500

    Partway through implementing controller to add new page.

---
+++ Web/scripts/route.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Commander Data on 10/27/2017.
+ */
+var routeApp = angular.module('routingApp', []); // might need ngRoute in []
+routeApp.config(function($routeProvider)) {
+    $routeProvider
+    .when('/pages/properties.html', {
+        templateUrl: 'pages/properties.html',
+        controller: 'propertiesController'
+    })
+
+    routeApp.controller('propertiesController', function($scope) {
+        $scope.propertiesMessage = 'Successful call to properties controller!';
+    });
+}
